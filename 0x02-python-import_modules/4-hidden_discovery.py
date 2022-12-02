13327d47941f1dcc08708206a4526d7287c9096a
OKPO-CHI CHIEMERIE <110995105+OSTAR1@users.noreply.github.com>
2022-12-02 20:58:13 +0100

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-#!/usr/bin/python3
-import sys
-import hidden_4 as hidden
-
-if __name__ != "__main__":
-    exit()
-
-    for name in dir(hidden):
-        if name[0:2] != "__":
-            print(name)
-=======
 #!/usr/bin/python3
 import sys
 import hidden_4 as hidden
@@ -20,4 +8,3 @@ if __name__ != "__main__":
 for name in dir(hidden):
     if name[0:2] != "__":
         print(name)
->>>>>>> 160a7a040f0dd3314313fd7d841a8d89a09cf2a3
